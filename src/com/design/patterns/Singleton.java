13f37c2a67c56f3b3ebc0d70c1f158da1afd9625
eliezers <Eliezer.Semyonov@redbend.com>
2016-09-08 15:15:32 +0300

    add private constructor.

--- src/com/design/patterns/Singleton.java
+++ src/com/design/patterns/Singleton.java
@@ -8,4 +8,14 @@ public class Singleton {
 
 
     private static Singleton instance;
+
+
+    //make the constructor private so that this class cannot be
+    //instantiated
+    private Singleton(){}
+
+
+    public static Singleton getInstance(){
+        return instance;
+    }
 }
